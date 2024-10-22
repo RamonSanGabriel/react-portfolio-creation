9ab499a222dec59d53bec81ba679e20b3b2b21c6
RamonSanGabriel <jeshero11@gmail.com>
2024-10-23 00:29:57 +0800

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,7 +5,7 @@ const HomePage = () => {
   return (
     <HelmetProvider>
       <Helmet>
-        <title>Juan Dela Cruz</title>
+        <title>Ramon San Gabriel</title>
       </Helmet>
 
       {/* Home Component */}
