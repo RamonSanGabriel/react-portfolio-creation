2c9c218def3f1e5784a90cb272f82242b2adba2e
RamonSanGabriel <jeshero11@gmail.com>
2024-10-25 17:16:24 +0800

    update about img

--- src/data/contact.jsx
+++ src/data/contact.jsx
@@ -13,9 +13,9 @@ export const contactInfo = [
           color: '#000000',
           fontSize: '20px',
         }}
-        href="tel:+639123456789"
+        href="tel:+639561705622"
       >
-        + 639123456789
+        +63 956 170 5622
       </a>
     ),
   },
